ee9002cdc5582317b8b4c5e8613c52f9a1377130
lockable-dev <b@lockable.dev>
2022-05-20 16:12:25 +0000

    Linting

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,16 +1,17 @@
-from lockable import Lock, hello
+from lockable import Lock
 import logging
 import time
+import uuid
 
 log = logging.getLogger(__name__)
 
 
 def test_lock(caplog):
-		caplog.set_level(logging.DEBUG, logger='lockable.lockable')
-		caplog.set_level(logging.DEBUG, logger=__name__)
-		import uuid
-		with Lock(str(uuid.uuid4())) as l:
-				log.debug('In with statement')
-				time.sleep(5)
-				pass
-		return
+    caplog.set_level(logging.DEBUG, logger='lockable.lock')
+    caplog.set_level(logging.DEBUG, logger='lockable.client')
+    caplog.set_level(logging.DEBUG, logger=__name__)
+    with Lock(str(uuid.uuid4())) as l:
+        log.debug('In with statement')
+        time.sleep(5)
+        pass
+    return
